c1b667ccb99a43c5b0d59fd515ab7b3c4e0f8eaa
BullShark <goodbye300@aim.com>
2013-06-13 08:41:24 -0500

    OnClickListener for the Button

--- AndroidTutorial/src/ch/expectusafterlun/androidtutorial/OpenedClass.java
+++ AndroidTutorial/src/ch/expectusafterlun/androidtutorial/OpenedClass.java
@@ -2,12 +2,14 @@ package ch.expectusafterlun.androidtutorial;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
-public class OpenedClass extends Activity {
+public class OpenedClass extends Activity implements OnClickListener {
 
 	private TextView tvQuestion, tvText;
 	private RadioGroup rgAnswers;
@@ -27,8 +29,13 @@ public class OpenedClass extends Activity {
 		rCrazy = (RadioButton) findViewById(R.id.r_crazy);
 		rAnnoying = (RadioButton) findViewById(R.id.r_annoying);
 		rBoth = (RadioButton) findViewById(R.id.r_both);
-		//TODO Why can't b_return be found?
 		bReturn = (Button) findViewById(R.id.b_return);
 		tvText = (TextView) findViewById(R.id.tv_text);
+		
+		bReturn.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
 	}
 }
