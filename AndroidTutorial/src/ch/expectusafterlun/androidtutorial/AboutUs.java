b6c4103b029b4a2cb90ef8c03b4925639e4aca64
BullShark <goodbye300@aim.com>
2013-06-15 18:58:13 -0500

    Activity for dialog launched from about us menu item

---
+++ AndroidTutorial/src/ch/expectusafterlun/androidtutorial/AboutUs.java
@@ -0,0 +1,13 @@
+package ch.expectusafterlun.androidtutorial;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutUs extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+	}
+}
