330cc6b6016eaa2f1ee1543101c0612a4f504495
BullShark <goodbye300@aim.com>
2013-06-19 10:13:17 -0500

    PreferenceActivity for the Preferences menu button

---
+++ AndroidTutorial/src/ch/expectusafterlun/androidtutorial/Prefs.java
@@ -0,0 +1,14 @@
+package ch.expectusafterlun.androidtutorial;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class Prefs extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.prefs);
+	}
+
+}
