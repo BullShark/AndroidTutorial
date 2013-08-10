943e44058d056f90d44df728db4897a3a082eed1
BullShark <goodbye300@aim.com>
2013-08-09 21:37:32 -0500

    The ball now moves with animation

--- AndroidTutorial/src/ch/expectusafterlun/androidtutorial/AnimationView.java
+++ AndroidTutorial/src/ch/expectusafterlun/androidtutorial/AnimationView.java
@@ -3,14 +3,33 @@ package ch.expectusafterlun.androidtutorial;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.view.View;
 
 public class AnimationView extends View {
 
-	Bitmap gBall;
+	private Bitmap gBall;
+	private float changingY;
 	
 	public AnimationView(Context context) {
 		super(context);
 		gBall = BitmapFactory.decodeResource(getResources(), R.drawable.greenball);
+		changingY = 0;
 	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+		canvas.drawColor(Color.WHITE);
+		// 100 pixels is the width of the green ball, 50 is half
+		canvas.drawBitmap(gBall, (canvas.getWidth()/2 - gBall.getScaledWidth(canvas)/2), changingY, null);
+		if(changingY < canvas.getHeight()) {
+			changingY += 10;
+		} else {
+			changingY = 10;
+		}
+		invalidate();
+	}
+	
 }
