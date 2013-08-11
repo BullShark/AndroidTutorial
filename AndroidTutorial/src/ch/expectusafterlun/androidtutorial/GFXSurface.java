cf9c93ddd99fe08dec6616d5b9f231a8debdaa29
BullShark <goodbye300@aim.com>
2013-08-10 20:39:20 -0500

    Implements an OnTouchListener to get x and y coordinates where touched

--- AndroidTutorial/src/ch/expectusafterlun/androidtutorial/GFXSurface.java
+++ AndroidTutorial/src/ch/expectusafterlun/androidtutorial/GFXSurface.java
@@ -2,30 +2,42 @@ package ch.expectusafterlun.androidtutorial;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 
-public class GFXSurface extends Activity {
+public class GFXSurface extends Activity implements OnTouchListener {
 	
 	AnimationViewSurface surfaceView;
+	float x, y;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		surfaceView = new AnimationViewSurface(this);
+		surfaceView.setOnTouchListener(this);
+		x = 0;
+		y = 0;
 		setContentView(surfaceView);
 	}
 
 	@Override
 	protected void onPause() {
-		// TODO Auto-generated method stub
 		super.onPause();
 		surfaceView.pause();
 	}
 
 	@Override
 	protected void onResume() {
-		// TODO Auto-generated method stub
 		super.onResume();
 		surfaceView.resume();
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		x = event.getX();
+		y = event.getY();
+		return false;
 	}
 
 }
