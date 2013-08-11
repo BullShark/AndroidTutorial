9d7c58fb9ef62ea6cd0ab1125ce9b14705048cc5
BullShark <goodbye300@aim.com>
2013-08-10 19:46:01 -0500

    Competed Video Tutorial 68
    Created a Thread and a SurfaceHolder

--- AndroidTutorial/src/ch/expectusafterlun/androidtutorial/AnimationViewSurface.java
+++ AndroidTutorial/src/ch/expectusafterlun/androidtutorial/AnimationViewSurface.java
@@ -1,10 +1,19 @@
 package ch.expectusafterlun.androidtutorial;
 
+import android.content.Context;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class AnimationViewSurface extends SurfaceView implements Runnable {
 
+	SurfaceHolder holder;
+	Thread thread;
+
 	public AnimationViewSurface(Context context) {
+		super(context);
+		holder = getHolder();
+		thread = new Thread(this);
+		thread.start();
 	}
 
 	@Override
