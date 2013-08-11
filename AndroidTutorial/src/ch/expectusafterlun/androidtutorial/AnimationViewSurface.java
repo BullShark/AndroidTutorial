ac55f5c6f7c7d02e4773f010497f167097c8ac07
BullShark <goodbye300@aim.com>
2013-08-10 20:04:21 -0500

    Waits for the canvas to become able to lock, locks and draws blue on it

--- AndroidTutorial/src/ch/expectusafterlun/androidtutorial/AnimationViewSurface.java
+++ AndroidTutorial/src/ch/expectusafterlun/androidtutorial/AnimationViewSurface.java
@@ -1,23 +1,34 @@
 package ch.expectusafterlun.androidtutorial;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class AnimationViewSurface extends SurfaceView implements Runnable {
 
-	SurfaceHolder holder;
-	Thread thread;
+	private SurfaceHolder holder;
+	private Thread thread;
+	private boolean isRunning;
 
 	public AnimationViewSurface(Context context) {
 		super(context);
 		holder = getHolder();
+		isRunning = true;
 		thread = new Thread(this);
 		thread.start();
 	}
 
 	@Override
 	public void run() {
+		while(isRunning) {
+			if(!holder.getSurface().isValid()) {
+				continue;
+			}
+			
+			Canvas canvas = holder.lockCanvas();
+			canvas.drawRGB(2, 2, 150);
+			holder.unlockCanvasAndPost(canvas);
+		}
 	}
-
 }
