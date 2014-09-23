e8dea3daed8bc3a23b1da0af86f7ba46d23e4177
Christopher Lemire <christopher.lemire@gmail.com>
2014-09-23 11:18:14 -0500

    Completed Video Tutorial 78
    FULL_WAKE_LOCK, preventing the screen from timing out only when the GFX Activity is the active one

--- AndroidTutorial/src/ch/expectusafterlun/androidtutorial/GFX.java
+++ AndroidTutorial/src/ch/expectusafterlun/androidtutorial/GFX.java
@@ -1,17 +1,40 @@
 package ch.expectusafterlun.androidtutorial;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.PowerManager;
 
 public class GFX extends Activity {
 
 	private AnimationView view;
-	
-	@Override
+    private PowerManager.WakeLock wl;
+
+  @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+    // Prevent the display from timing out/turning off
+    // Why does video 78 show super.onCreate(Bundle) being called directly after PowerManager?
+    PowerManager pm = (PowerManager)getSystemService(Context.POWER_SERVICE);
+    wl = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK, "FULL_WAKE_LOCK Demo: Preventing the display from turning off" +
+        "while this Activity is active");
+    wl.acquire();
+
 		view = new AnimationView(this);
 		setContentView(view);
 	}
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    wl.acquire();
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    wl.release();
+  }
 
 }
