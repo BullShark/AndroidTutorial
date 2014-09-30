4349e7eee28cf5276928a557958a8bd1b5895d1c
Christopher Lemire <christopher.lemire@gmail.com>
2014-09-29 23:02:17 -0500

    Dddemonstrates SoundPool for short sounds/audio clips

--- AndroidTutorial/src/ch/expectusafterlun/androidtutorial/SoundStuff.java
+++ AndroidTutorial/src/ch/expectusafterlun/androidtutorial/SoundStuff.java
@@ -1,7 +1,36 @@
 package ch.expectusafterlun.androidtutorial;
 
+import android.app.Activity;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.view.View;
+
 /**
- * Created by Tux on 9/23/2014.
+ * Created by Christopher Lemire on 9/23/2014.
  */
-public class SoundStuff {
+public class SoundStuff extends Activity implements View.OnClickListener {
+  // SoundPool is for shorter sounds/audio clips as
+  // Opposed to MediaPlayer for longer sounds
+  SoundPool sp;
+  int explosion = 0;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    View view = new View(this);
+    view.setOnClickListener(this);
+    setContentView(view);
+    // Up to 5 explosions will be playable simultaneously
+    sp = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
+    explosion = sp.load(this, R.raw.grenade, 1);
+  }
+
+  @Override
+  public void onClick(View v) {
+    // Prevent possibly playing before explosion has loaded
+    if(explosion != 0) {
+      sp.play(explosion, 1, 1, 0, 0, 1);
+    }
+  }
 }
