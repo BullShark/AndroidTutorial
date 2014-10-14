5d91c5adb140acbfdb74106f869fa055621c89a5
Christopher Lemire <christopher.lemire@gmail.com>
2014-10-14 14:25:25 -0500

    Implement Listening Interfaces and override their methods for Components

--- AndroidTutorial/src/ch/expectusafterlun/androidtutorial/Slider.java
+++ AndroidTutorial/src/ch/expectusafterlun/androidtutorial/Slider.java
@@ -2,13 +2,15 @@ package ch.expectusafterlun.androidtutorial;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 /**
  * Created by Christopher Lemire 9/30/2014.
  */
-public class Slider extends Activity {
+public class Slider extends Activity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -19,5 +21,15 @@ public class Slider extends Activity {
     Button handle3 = (Button) findViewById(R.id.handle3);
     Button handle4 = (Button) findViewById(R.id.handle4);
     CheckBox checkbox = (CheckBox) findViewById(R.id.cb_slidable);
+  }
+
+  @Override
+  public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+  }
+
+  @Override
+  public void onClick(View v) {
+
   }
 }
