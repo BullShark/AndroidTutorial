3c3d19b70dc361cc02ae83042de568fb7a1fa08c
Christopher Lemire <christopher.lemire@gmail.com>
2014-10-14 14:21:31 -0500

    Set up reference bridges for GUI components from XML to Java

--- AndroidTutorial/src/ch/expectusafterlun/androidtutorial/Slider.java
+++ AndroidTutorial/src/ch/expectusafterlun/androidtutorial/Slider.java
@@ -2,6 +2,8 @@ package ch.expectusafterlun.androidtutorial;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 /**
  * Created by Christopher Lemire 9/30/2014.
@@ -11,5 +13,11 @@ public class Slider extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.sliding);
+    Button handle0 = (Button) findViewById(R.id.handle0);
+    Button handle1 = (Button) findViewById(R.id.handle1);
+    Button handle2 = (Button) findViewById(R.id.handle2);
+    Button handle3 = (Button) findViewById(R.id.handle3);
+    Button handle4 = (Button) findViewById(R.id.handle4);
+    CheckBox checkbox = (CheckBox) findViewById(R.id.cb_slidable);
   }
 }
