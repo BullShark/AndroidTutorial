f7f6bd99f825963f7defdad4ebe6dd118b6eb672
Christopher Lemire <goodbye300@aim.com>
2021-09-14 00:29:03 -0500

    Completed Video Tutorial 84

--- app/src/main/java/ch/expectusafterlun/androidtutorial/Tabs.java
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/Tabs.java
@@ -1,10 +1,16 @@
 package ch.expectusafterlun.androidtutorial;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by Christopher Lemire
  */
-public class Tabs {
+public class Tabs extends Activity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tabs);
+    }
 }
