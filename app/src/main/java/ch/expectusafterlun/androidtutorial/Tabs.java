b2ad53406e430770873b799601b3ccc173c97a4c
Christopher Lemire <goodbye300@aim.com>
2021-09-15 03:44:56 -0500

    Completed Video Tutorial 85

--- app/src/main/java/ch/expectusafterlun/androidtutorial/Tabs.java
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/Tabs.java
@@ -2,6 +2,8 @@ package ch.expectusafterlun.androidtutorial;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
 /**
  * Created by Christopher Lemire
@@ -12,5 +14,22 @@ public class Tabs extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tabs);
+
+        TabHost th = (TabHost) findViewById(R.id.tabhost);
+        th.setup();
+        TabSpec tabSpec = th.newTabSpec("tag1");
+        tabSpec.setContent(R.id.tab1);
+        tabSpec.setIndicator("StopWatch");
+        th.addTab(tabSpec);
+
+        tabSpec = th.newTabSpec("tag2");
+        tabSpec.setContent(R.id.tab2);
+        tabSpec.setIndicator("Tab 2");
+        th.addTab(tabSpec);
+
+        tabSpec = th.newTabSpec("tag3");
+        tabSpec.setContent(R.id.tab3);
+        tabSpec.setIndicator("Add a Tab");
+        th.addTab(tabSpec);
     }
 }
