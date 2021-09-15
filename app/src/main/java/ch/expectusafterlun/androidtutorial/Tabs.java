23b34667c1fc2e1f1841002b876de52706d8b6b3
Christopher Lemire <goodbye300@aim.com>
2021-09-15 04:11:15 -0500

    Completed Video Tutorial 86

--- app/src/main/java/ch/expectusafterlun/androidtutorial/Tabs.java
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/Tabs.java
@@ -2,21 +2,31 @@ package ch.expectusafterlun.androidtutorial;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
 
 /**
  * Created by Christopher Lemire
  */
-public class Tabs extends Activity {
+public class Tabs extends Activity implements View.OnClickListener {
 
+    TabHost th;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tabs);
 
-        TabHost th = (TabHost) findViewById(R.id.tabhost);
+        th = (TabHost) findViewById(R.id.tabhost);
         th.setup();
+        Button newTab = (Button) findViewById(R.id.btn_add_tab);
+        Button btnStart = (Button) findViewById(R.id.btn_start_watch);
+        Button btnStop = (Button) findViewById(R.id.btn_stop_watch);
+
+        newTab.setOnClickListener(this);
+
         TabSpec tabSpec = th.newTabSpec("tag1");
         tabSpec.setContent(R.id.tab1);
         tabSpec.setIndicator("StopWatch");
@@ -31,5 +41,33 @@ public class Tabs extends Activity {
         tabSpec.setContent(R.id.tab3);
         tabSpec.setIndicator("Add a Tab");
         th.addTab(tabSpec);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.btn_add_tab:
+
+                TabSpec tabSpec = th.newTabSpec("tag1");
+                tabSpec.setContent(new TabHost.TabContentFactory() {
+
+                    public View createTabContent(String tag) {
+
+                        TextView tv = new TextView(Tabs.this);
+                        tv.setText("You've created a new Tab!");
+                        return tv;
+                    }
+                });
+                tabSpec.setIndicator("New");
+                th.addTab(tabSpec);
+
+                break;
+
+            case R.id.btn_start_watch:
+                break;
+
+            case R.id.btn_stop_watch:
+                break;
+        }
     }
 }
