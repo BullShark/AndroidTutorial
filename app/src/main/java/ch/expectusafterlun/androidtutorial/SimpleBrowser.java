cb59115b59764f730887e3cebe07722a0acae978
Christopher Lemire <goodbye300@aim.com>
2021-11-02 22:35:53 -0500

    Completed Video Tutorial 89

--- app/src/main/java/ch/expectusafterlun/androidtutorial/SimpleBrowser.java
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/SimpleBrowser.java
@@ -3,6 +3,7 @@ package ch.expectusafterlun.androidtutorial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class SimpleBrowser extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class SimpleBrowser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.simplebrowser);
+
+        WebView browser = (WebView) findViewById(R.id.wv_browser);
+        browser.loadUrl("https://expectusafterlun.ch");
     }
 }
