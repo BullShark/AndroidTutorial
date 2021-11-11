470e2f4bad2469a56bb1fbc9df4ffca127b10634
Christopher Lemire <goodbye300@aim.com>
2021-11-11 12:05:45 -0600

    Solved all the problem errors the IDE was pointing out and took care of many of the warnings Also gradle files

--- app/src/main/java/ch/expectusafterlun/androidtutorial/InternalData.java
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/InternalData.java
@@ -2,21 +2,22 @@ package ch.expectusafterlun.androidtutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
-public class SharedPrefs extends AppCompatActivity {
+public class InternalData extends AppCompatActivity implements View.OnClickListener {
 
     private EditText sharedData;
     private TextView dataResult;
-    public static final String FILENAME = "SHARED";
-    private SharedPreferences prefs;
+    public static final String FILENAME = "Internal";
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sharedpreferences);
@@ -27,24 +28,25 @@ public class SharedPrefs extends AppCompatActivity {
         Button save = (Button) findViewById(R.id.bSave);
         Button load = (Button) findViewById(R.id.bLoad);
         sharedData = (EditText) findViewById(R.id.etSharedPrefs);
-        dataResult = (TextView) findViewById(R.id.tvLoadSharedPreferences);
         save.setOnClickListener((View.OnClickListener) this);
         load.setOnClickListener((View.OnClickListener) this);
-        prefs = getSharedPreferences(FILENAME, 0);
+        SharedPreferences prefs = getSharedPreferences(FILENAME, 0);
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    @Override
     public void onClick(View v) {
         switch(v.getId()) {
             case R.id.bSave:
-                String data = sharedData.getText().toString();
-                SharedPreferences.Editor editor = prefs.edit();
-                editor.putString("shared", data);
-                editor.apply();
+
                 break;
             case R.id.bLoad:
-                prefs = getSharedPreferences(FILENAME, 0);
-                String results = prefs.getString("shared", "Couldn't load data!");
-                dataResult.setText(results);
+
                 break;
             default:
                 throw new IllegalStateException("Unexpected value: " + v.getId());
