ecead9403f43e8bc7f8d2d8f2ce890255fe625d3
Christopher Lemire <goodbye300@aim.com>
2021-11-14 05:56:50 -0600

    Completed Video Tutorial 104

--- app/src/main/java/ch/expectusafterlun/androidtutorial/ExternalData.java
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/ExternalData.java
@@ -3,11 +3,13 @@ package ch.expectusafterlun.androidtutorial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.widget.TextView;
 
 public class ExternalData extends AppCompatActivity {
 
     private TextView canWrite, canRead;
+    private String state;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +17,18 @@ public class ExternalData extends AppCompatActivity {
         setContentView(R.layout.externaldata);
         canWrite = (TextView)  findViewById(R.id.tvCanWrite);
         canRead = (TextView)  findViewById(R.id.tvCanRead);
+        state = Environment.getExternalStorageState();
+        if(state.equals(Environment.MEDIA_MOUNTED)) {
+            // Read and write
+            canWrite.setText("Can write: true");
+            canRead.setText("Can reads: true");
+        } else if(state.equals(Environment.MEDIA_MOUNTED_READ_ONLY)) {
+            // Read but can't write
+            canWrite.setText("Can write: false");
+            canRead.setText("Can reads: true");
+        } else {
+            canWrite.setText("Can write: false");
+            canRead.setText("Can reads: false");
+        }
     }
 }
