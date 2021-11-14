2c81223e6cb33f673a7a2cfb6ad023dec757a36f
Christopher Lemire <goodbye300@aim.com>
2021-11-14 06:20:06 -0600

    Completed Video Tutorial 105

--- app/src/main/java/ch/expectusafterlun/androidtutorial/ExternalData.java
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/ExternalData.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Environment;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class ExternalData extends AppCompatActivity {
 
     private TextView canWrite, canRead;
     private String state;
+    private boolean canW, canR;
+    private Spinner spinner;
+    private String[] paths = { "Music", "Pictures", "Download" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +27,23 @@ public class ExternalData extends AppCompatActivity {
             // Read and write
             canWrite.setText("Can write: true");
             canRead.setText("Can reads: true");
+            canW = canR = true;
         } else if(state.equals(Environment.MEDIA_MOUNTED_READ_ONLY)) {
             // Read but can't write
             canWrite.setText("Can write: false");
             canRead.setText("Can reads: true");
+            canW = false;
+            canR = true;
         } else {
             canWrite.setText("Can write: false");
             canRead.setText("Can reads: false");
+            canW = canR = false;
         }
+
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(ExternalData.this, android.R.layout.simple_spinner_item, paths);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setAdapter(adapter);
     }
 }
