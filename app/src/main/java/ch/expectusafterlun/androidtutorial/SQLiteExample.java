d74d5491c6638281571bd1bdd1dffcdf8e46aadb
Christopher Lemire <goodbye300@aim.com>
2021-11-17 02:56:33 -0600

    Completed Video Tutorial 111

---
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/SQLiteExample.java
@@ -0,0 +1,40 @@
+package ch.expectusafterlun.androidtutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SQLiteExample extends AppCompatActivity implements View.OnClickListener {
+
+    private Button sqlUpdate, sqlView;
+    private EditText sqlName, sqlHotness;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sqliteexample);
+
+        sqlUpdate = (Button) findViewById(R.id.b_sql_update);
+        sqlName = (EditText) findViewById(R.id.et_sql_name);
+        sqlHotness = (EditText) findViewById(R.id.et_sql_hotness);
+        sqlView = (Button) findViewById(R.id.b_sql_open_view);
+
+        sqlView.setOnClickListener(this);
+        sqlUpdate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.b_sql_update:
+
+                break;
+            case R.id.b_sql_open_view:
+
+                break;
+        }
+    }
+}
