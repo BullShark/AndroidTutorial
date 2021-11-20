bbabeb6b83e9133a45dc5cee7af3f20588339beb
Christopher Lemire <goodbye300@aim.com>
2021-11-20 14:53:32 -0600

    Completed Video Tutorial 126

--- app/src/main/java/ch/expectusafterlun/androidtutorial/Accelerate.java
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/Accelerate.java
@@ -7,6 +7,7 @@ import android.graphics.Canvas;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -19,15 +20,25 @@ public class Accelerate extends AppCompatActivity implements SensorEventListener
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new AnimationViewSurface(this));
+        // Check if the device has an accelerometer
+        SensorManager sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if(sm.getSensorList(Sensor.TYPE_ACCELEROMETER).size() != 0) {
+            Sensor sensor = sm.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0);
+            sm.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        // Have the ball move around the screen when the sensor changes
+        // This gets called when we have a new SensorEvent,
+        // When our accelerometer changes directions
 
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Test the accuracy of the changed event
 
     }
 
