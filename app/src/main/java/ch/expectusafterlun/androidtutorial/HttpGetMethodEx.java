4d2388a928a8e6d4bb16fbfe2c09b6700a695170
Christopher Lemire <goodbye300@aim.com>
2021-11-28 18:40:04 -0600

    Completed Video Tutorial 148

--- app/src/main/java/ch/expectusafterlun/androidtutorial/HttpGetMethodEx.java
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/HttpGetMethodEx.java
@@ -1,10 +1,15 @@
 package ch.expectusafterlun.androidtutorial;
 
+import android.net.http.HttpResponseCache;
+
+import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
 
 public class HttpGetMethodEx {
 
@@ -14,6 +19,14 @@ public class HttpGetMethodEx {
 
         try {
             HttpClient client = new DefaultHttpClient();
+            URI website = new URI("http://www.mybringback.com");
+            HttpGet request = new HttpGet();
+            request.setURI(website);
+            HttpResponse response = client.execute(request);
+
+            in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+
         } catch(Exception e) {
             e.printStackTrace();
         }
