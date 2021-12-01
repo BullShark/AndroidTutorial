7ca16d9bb0ee395b7328262b0f541e87af8360d2
Christopher Lemire <goodbye300@aim.com>
2021-12-01 05:10:36 -0600

    Completed Video Tutorial 158

--- app/src/main/java/ch/expectusafterlun/androidtutorial/HandlingXMLStuff.java
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/HandlingXMLStuff.java
@@ -1,4 +1,21 @@
 package ch.expectusafterlun.androidtutorial;
 
-public class HandlingXMLStuff {
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class HandlingXMLStuff extends DefaultHandler {
+
+    private XMLDataCollected info = new XMLDataCollected();
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        if(localName.equals("city")) {
+            String city = attributes.getValue("name");
+            info.setCity(city);
+        } else if(localName.equals("temperature")) {
+            String temperature = attributes.getValue("value");
+            info.setTemp(Integer.parseInt(temperature));
+        }
+    }
 }
