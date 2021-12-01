4118f3236bfdcf8e906dac19830807ebf5ebebcf
Christopher Lemire <goodbye300@aim.com>
2021-12-01 04:49:56 -0600

    Completed Video Tutorial 156

--- app/src/main/java/ch/expectusafterlun/androidtutorial/XMLDataCollected.java
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/XMLDataCollected.java
@@ -1,4 +1,18 @@
 package ch.expectusafterlun.androidtutorial;
 
 public class XMLDataCollected {
+    int temperature = 0;
+    String city = null;
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setTemp(int temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getData() {
+        return "In " + city + " the Current Temperature in F is " + temperature + " degrees";
+    }
 }
