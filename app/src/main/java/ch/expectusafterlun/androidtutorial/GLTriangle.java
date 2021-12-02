a474a1a6aeb2c237fa5818333068a99125e9a3ac
Christopher Lemire <goodbye300@aim.com>
2021-12-02 15:13:16 -0600

    Completed Video Tutorial 173

--- app/src/main/java/ch/expectusafterlun/androidtutorial/GLTriangle.java
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/GLTriangle.java
@@ -3,6 +3,7 @@ package ch.expectusafterlun.androidtutorial;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
 
 public class GLTriangle {
     /* x and y coordinates for point 1, 2 and 3 */
@@ -14,6 +15,11 @@ public class GLTriangle {
 
     private FloatBuffer vertBuff;
 
+    /* Point Index and Point Buffer */
+    private short[] pIndex = { 0, 1, 2 };
+
+    private ShortBuffer pBuff;
+
     /* Each float takes up 4 bytes
      * We need to reserve 24 bytes
      */
@@ -23,5 +29,11 @@ public class GLTriangle {
         vertBuff = bBuff.asFloatBuffer();
         vertBuff.put(VERTICES);
         vertBuff.position(0);
+
+        ByteBuffer pbBuff = ByteBuffer.allocateDirect(pIndex.length * 2);
+        pbBuff.order(ByteOrder.nativeOrder());
+        pBuff = pbBuff.asShortBuffer();
+        pBuff.put(pIndex);
+        pBuff.position(0);
     }
 }
