4e569faa25f930c64e5d4803d80ec5e8c2132b80
Christopher Lemire <goodbye300@aim.com>
2021-12-02 14:22:38 -0600

    Completed Video Tutorial 170

--- app/src/main/java/ch/expectusafterlun/androidtutorial/GLRenderer.java
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/GLRenderer.java
@@ -8,12 +8,13 @@ import javax.microedition.khronos.opengles.GL10;
 public class GLRenderer implements GLSurfaceView.Renderer {
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-
+        gl.glClearColor(.8f, 0, .2f, 1);
+        gl.glClearDepthf(1f);
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
-
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
     }
 
     @Override
