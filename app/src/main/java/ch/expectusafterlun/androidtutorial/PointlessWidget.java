9f298afa63d08b2a3c67e847a2e336da2e76cf4c
Christopher Lemire <goodbye300@aim.com>
2021-12-02 00:44:33 -0600

    Completed Video Tutorial 163

--- app/src/main/java/ch/expectusafterlun/androidtutorial/PointlessWidget.java
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/PointlessWidget.java
@@ -1,4 +1,25 @@
 package ch.expectusafterlun.androidtutorial;
 
-public class PointlessWidget {
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class PointlessWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        Random r = new Random();
+        int randomInt = r.nextInt(10000000);
+        String rand = String.valueOf(randomInt);
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+        Toast.makeText(context, "See ya sucka!", Toast.LENGTH_SHORT).show();
+    }
 }
