5d7af2f61dd5741670da0c74814869df61e079d1
Christopher Lemire <goodbye300@aim.com>
2021-12-02 20:29:34 -0600

    Completed Video Tutorial 176

--- app/src/main/java/ch/expectusafterlun/androidtutorial/GLRenderer.java
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/GLRenderer.java
@@ -6,15 +6,27 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 public class GLRenderer implements GLSurfaceView.Renderer {
+
+    private GLTriangle tri;
+
+    public GLRenderer() {
+        tri = new GLTriangle();
+    }
+
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        gl.glDisable(GL10.GL_DITHER); /* Improve performance */
+        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_FASTEST);
         gl.glClearColor(.8f, 0, .2f, 1);
         gl.glClearDepthf(1f);
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
+        gl.glDisable(GL10.GL_DITHER);
         gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+
+        tri.draw(gl);
     }
 
     @Override
