5422ace2645691737ab412c4a4b4171bf114975c
Christopher Lemire <goodbye300@aim.com>
2022-01-18 16:38:30 -0600

    Completed Video Tutorial 192

---
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/SeekBarVolume.java
@@ -0,0 +1,48 @@
+package ch.expectusafterlun.androidtutorial;
+
+import android.app.Activity;
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.widget.SeekBar;
+
+public class SeekBarVolume extends Activity implements SeekBar.OnSeekBarChangeListener {
+
+    private SeekBar sb;
+    private MediaPlayer mp;
+    private AudioManager am;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.seekbarvolume);
+        sb = (SeekBar) findViewById(R.id.sb_volume);
+        mp = MediaPlayer.create(this, R.raw.dubstep);
+        mp.start();
+        am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int maxV = am.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int curV = am.getStreamVolume(AudioManager.STREAM_MUSIC);
+        sb.setOnSeekBarChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mp.release();
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+}
