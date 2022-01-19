b512bfa16d0ee528d106f50592a93844280f4c11
Christopher Lemire <goodbye300@aim.com>
2022-01-18 19:00:44 -0600

    AppCompatButton, AppCompat Themes, AppCompatActivity

--- app/src/main/java/ch/expectusafterlun/androidtutorial/TextVoice.java
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/TextVoice.java
@@ -1,27 +1,27 @@
 package ch.expectusafterlun.androidtutorial;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import java.util.Locale;
 import java.util.Random;
 
-public class TextVoice extends Activity implements View.OnClickListener {
+public class TextVoice extends AppCompatActivity implements View.OnClickListener {
 
     private static final String[] texts = {
         "Whaaat's up Gangstas!", "You smell!", "Supersize it!"
     };
     private TextToSpeech tts;
+    private AppCompatButton b;
 
     protected void OnCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.textvoice);
-        Button b = (Button) findViewById(R.id.b_text_to_voice);
+        b = (AppCompatButton) findViewById(R.id.b_text_to_voice);
         b.setOnClickListener(this);
         tts = new TextToSpeech(TextVoice.this, new TextToSpeech.OnInitListener() {
             public void onInit(int status) {
