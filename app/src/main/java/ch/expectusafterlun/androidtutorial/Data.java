b568e627dc457ea435dcec1bc045242daf365884
Christopher Lemire <goodbye300@aim.com>
2022-02-02 17:12:49 -0600

    Completed Video Tutorial 198

--- app/src/main/java/ch/expectusafterlun/androidtutorial/Data.java
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/Data.java
@@ -3,6 +3,7 @@ package ch.expectusafterlun.androidtutorial;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -10,9 +11,14 @@ import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.RequestConfiguration;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+
+import java.util.Arrays;
 
 public class Data extends Activity implements OnClickListener {
 
@@ -24,45 +30,70 @@ public class Data extends Activity implements OnClickListener {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.receive);
 		initialize();
 
 		// Create a banner ad. The ad size and ad unit ID must be set before calling loadAd.
 		rl = (RelativeLayout) findViewById(R.id.rel_layout);
-		AdView ad = new AdView(this);
-		ad.setAdSize(AdSize.BANNER);
-		ad.setAdUnitId(String.valueOf(R.string.banner_ad_unit_id));
-		rl.addView(ad);
-		ad.loadAd(new AdRequest.Builder().build());
+		AdView ad = new AdView(Data.this);
+		// Create an ad request.
+		AdRequest request = new AdRequest.Builder()
+				//.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)        // All emulators
+				//.addTestDevice("17281JECB02717")  // My Google Pixel Phone
+				.build();
 
-		setContentView(R.layout.receive);
+		new RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("13B6DBC07B8192BE1B8441B62BCCFAA2"));
+
+		/*
+		List<String> testDeviceIds = Arrays.asList("13B6DBC07B8192BE1B8441B62BCCFAA2");
+		RequestConfiguration configuration =
+				new RequestConfiguration.Builder().setTestDeviceIds(testDeviceIds).build();
+		MobileAds.setRequestConfiguration(configuration);
+		*/
+
+		ad.setAdSize(AdSize.BANNER);
+		ad.setAdUnitId(String.valueOf(R.string.banner_ad_unit_id_test));
+		rl.addView(ad);
+		ad.loadAd(request);
+
 	}
 
 	private void initialize() {
 		etSend = (EditText) findViewById(R.id.et_send);
-		bStart = (Button) findViewById(R.id.b_sa);
-		bStartFor = (Button) findViewById(R.id.b_safr);
+		bStart = (Button) findViewById(R.id.b_start_activity);
+		bStartFor = (Button) findViewById(R.id.b_start_activity_for_result);
 		tvAnswer = (TextView) findViewById(R.id.tv_received);
+
 		// Set Listeners
-		bStart.setOnClickListener(this);
-		bStartFor.setOnClickListener(this);
+		if(bStart == null) {
+			Log.e("Data Button", "bStart is null!");
+		} else {
+			bStart.setOnClickListener(this);
+		}
+
+		if(bStartFor == null) {
+			Log.e("Data Button", "bStartFor is null!");
+		} else {
+			bStartFor.setOnClickListener(this);
+		}
 	}
 
 	@Override
 	public void onClick(View v) {
-		switch(v.getId()) {
-		case R.id.b_sa:
-			String bread = etSend.getText().toString();
-			Bundle basket =  new Bundle();
-			// Putting the bread in the basket
-			basket.putString("key", bread);
-			Intent a = new Intent(Data.this, Opens.class);
-			a.putExtras(basket);
-			startActivity(a);
-			break;
-		case R.id.b_safr:
-			Intent i = new Intent(Data.this, Opens.class);
-			startActivityForResult(i, 0); // 0 is the default
-			break;
+		switch (v.getId()) {
+			case R.id.b_start_activity:
+				String bread = etSend.getText().toString();
+				Bundle basket = new Bundle();
+				// Putting the bread in the basket
+				basket.putString("key", bread);
+				Intent a = new Intent(Data.this, Opens.class);
+				a.putExtras(basket);
+				startActivity(a);
+				break;
+			case R.id.b_start_activity_for_result:
+				Intent i = new Intent(Data.this, Opens.class);
+				startActivityForResult(i, 0); // 0 is the default
+				break;
 		}
 	}
 
--- app/src/main/java/ch/expectusafterlun/androidtutorial/Voice.java
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/Voice.java
@@ -7,23 +7,26 @@ import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.RequestConfiguration;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Voice extends Activity implements View.OnClickListener {
 
     private ListView lv;
     private static final int check = 1111;
+    //private LinearLayout ll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,15 +36,16 @@ public class Voice extends Activity implements View.OnClickListener {
         Button b = (Button) findViewById(R.id.b_voice);
         b.setOnClickListener(this);
 
-        MobileAds.initialize(this, new OnInitializationCompleteListener() {
-
-            @Override
-            public void onInitializationComplete(@NonNull InitializationStatus initializationStatus) {
-            }
-        });
-
+        //ll = (LinearLayout) findViewById(R.id.linear_layout1);
         AdView mAdView = (AdView)findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
+        RequestConfiguration configuration =
+                new RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("13B6DBC07B8192BE1B8441B62BCCFAA2")).build();
+        MobileAds.setRequestConfiguration(configuration);
+
+        mAdView.setAdUnitId(String.valueOf(R.string.banner_ad_unit_id_test));
+        mAdView.setAdSize(AdSize.BANNER);
+        //ll.addView(mAdView);
         mAdView.loadAd(adRequest);
 
     }
