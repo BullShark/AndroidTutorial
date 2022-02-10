03ea52f12c8a0fb2ccac8c3de8957086f702081a
Christopher Lemire <goodbye300@aim.com>
2022-02-10 05:32:13 -0600

    Completed Video Tutorial 29

--- app/src/main/java/ch/expectusafterlun/androidtutorial/BottomPictureFragment.java
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/BottomPictureFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,10 +16,23 @@ import androidx.fragment.app.Fragment;
  * 2) Needs to override onCreateView(...)
  */
 public class BottomPictureFragment extends Fragment {
+
+    private static TextView topMemeText;
+    private static TextView bottomMemeText;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.bottom_picture_fragment, container, false);
+
+        topMemeText = (TextView) view.findViewById(R.id.tv_top_meme_text);
+        bottomMemeText = (TextView) view.findViewById(R.id.tv_bottom_meme_text);
+
         return view;
+    }
+
+    public void setMemeText(String top, String bottom) {
+        topMemeText.setText(top);
+        bottomMemeText.setText(bottom);
     }
 }
--- app/src/main/java/ch/expectusafterlun/androidtutorial/MemeCreator.java
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/MemeCreator.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MemeCreator extends AppCompatActivity {
+public class MemeCreator extends AppCompatActivity implements TopSectionFragment.TopSectionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.meme);
+    }
+
+    // This gets called by the Top Fragment when the user clicks the button.
+    @Override
+    public void createMeme(String top, String bottom) {
+
     }
 }
