efce75cc68e13733cfe7219c72599d8c333d649e
Christopher Lemire <goodbye300@aim.com>
2022-02-10 00:10:54 -0600

    Completed Video Tutorial 17

---
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/GridLayoutExample.java
@@ -0,0 +1,14 @@
+package ch.expectusafterlun.androidtutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GridLayoutExample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.grid);
+    }
+}
