66ca0886b256a75881c35f862878ba517ee3596f
Christopher Lemire <goodbye300@aim.com>
2022-02-09 22:32:28 -0600

    Completed Video Tutorial 15

--- app/src/main/java/ch/expectusafterlun/androidtutorial/Allison.java
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/Allison.java
@@ -2,14 +2,17 @@ package ch.expectusafterlun.androidtutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.widget.RelativeLayout;
 import android.widget.Button;
+import android.widget.EditText;
 
 import android.os.Bundle;
 
 public class Allison extends AppCompatActivity {
 
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +20,7 @@ public class Allison extends AppCompatActivity {
         // Layout
         RelativeLayout rl = new RelativeLayout(this);
         Button redButton = new Button(this);
+        EditText etUserName = new EditText(this);
 
         // Rules
         RelativeLayout.LayoutParams buttonDetails = new RelativeLayout.LayoutParams(
@@ -25,14 +29,24 @@ public class Allison extends AppCompatActivity {
         );
         buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
         buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+        RelativeLayout.LayoutParams userNameDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        userNameDetails.addRule(RelativeLayout.ABOVE, redButton.getId());
+        userNameDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        userNameDetails.setMargins(0,0,0, 50);
 
         // Attributes
-        redButton.setText("Push me!");
+        redButton.setId(1);
+        etUserName.setId(2);
+        redButton.setText("Log In");
         rl.setBackgroundColor(Color.GREEN);
         redButton.setBackgroundColor(Color.RED);
 
         // Add widget to layout
         rl.addView(redButton, buttonDetails);
+        rl.addView(etUserName, userNameDetails);
 
         // Set this Activities content(display) to this View
         setContentView(rl);
