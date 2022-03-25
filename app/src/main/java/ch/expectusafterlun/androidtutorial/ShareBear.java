2bf837f3fdfe9c404f6b5d06ddacc0bfc48f837d
Christopher Lemire <goodbye300@aim.com>
2022-03-25 17:59:47 -0500

    Completed Video Tutorial 66

--- app/src/main/java/ch/expectusafterlun/androidtutorial/ShareBear.java
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/ShareBear.java
@@ -1,10 +1,13 @@
 package ch.expectusafterlun.androidtutorial;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ShareBear extends Activity {
 
@@ -21,9 +24,28 @@ public class ShareBear extends Activity {
         buckysText = findViewById(R.id.buckysText);
     }
 
+    // Save the user's login info
     public void saveInfo(View view) {
+        SharedPreferences sharedPref = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("username", usernameInput.getText().toString());
+        editor.putString("password", passwordInput.getText().toString());
+        editor.apply();
+
+        usernameInput.setText("");
+        passwordInput.setText("");
+
+        Toast.makeText(this, "Saved!", Toast.LENGTH_LONG).show();
     }
 
+    // Print out the saved data
     public void displayData(View view) {
+        SharedPreferences sharedPref = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+
+        String name = sharedPref.getString("username", "");
+        String password = sharedPref.getString("password", "");
+
+        buckysText.setText(name + ", " + password);
     }
 }
