9f01c54cc4b6c3864a2c947b772fabd98190c0a8
Christopher Lemire <goodbye300@aim.com>
2022-03-25 04:59:46 -0500

    Completed Video Tutorial 49

---
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/SQLiteSample.java
@@ -0,0 +1,22 @@
+package ch.expectusafterlun.androidtutorial;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SQLiteSample extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sqlite_sample);
+    }
+
+    public void addButtonClicked(View view) {
+
+    }
+
+    public void deleteButtonClicked(View view) {
+
+    }
+}
