cce3b5caebb2dd4592744e629d8dd62af3902fdb
Christopher Lemire <goodbye300@aim.com>
2022-03-24 21:56:55 -0500

    Completed Video Tutorial 35

---
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/Apples.java
@@ -0,0 +1,17 @@
+package ch.expectusafterlun.androidtutorial;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Apples extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.apples);
+    }
+
+    public void onClick() {
+
+    }
+}
