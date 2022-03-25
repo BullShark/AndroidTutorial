543ece5924eb0684790cdbec11bb6301e21870c2
Christopher Lemire <goodbye300@aim.com>
2022-03-24 22:40:57 -0500

    Completed Video Tutorial 37

---
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/SendBroadcast.java
@@ -0,0 +1,26 @@
+package ch.expectusafterlun.androidtutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.view.View;
+
+public class SendBroadcast extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.send_broadcast);
+    }
+
+    public void sendOutBroadcast(View view) {
+        Intent i = new Intent();
+        // We only want to listen for these intents/broadcast
+        i.setAction("ch.expectusafterlun.androidtutorial");
+        // Make this compatible with all versions of Android
+        i.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        sendBroadcast(i);
+    }
+}
