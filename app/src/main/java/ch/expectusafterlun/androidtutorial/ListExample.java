4ccf7ec2b6049a0deb4dbe64fd3ac03bfe9594b6
Christopher Lemire <goodbye300@aim.com>
2022-03-25 02:47:08 -0500

    Completed Video Tutorial 46

---
+++ app/src/main/java/ch/expectusafterlun/androidtutorial/ListExample.java
@@ -0,0 +1,35 @@
+package ch.expectusafterlun.androidtutorial;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class ListExample extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_example);
+
+        String[] foods = {"Bacon", "Ham", "Tuna", "Candy", "Meatball", "Potato"};
+        ListAdapter adapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, foods);
+        ListView listView = findViewById(R.id.chrisListView);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(
+            new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    String food = String.valueOf(parent.getItemAtPosition(position));
+                    Toast.makeText(ListExample.this, food, Toast.LENGTH_LONG).show();
+                }
+            }
+        );
+    }
+}
